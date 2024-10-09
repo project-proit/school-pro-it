7e2a2b94a23292a31a2d0ec878754ce32f1433de
YanaFicus <166223418+YanaFicus@users.noreply.github.com>
2024-10-09 03:52:09 +0300

    slepp

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,33 +16,38 @@ function Footer() {
     <div className='footer'>
         <div className='footer-container'>
             <div className='content-left'>
-                <div  className='contacts'>
-                    <h1>Контакты</h1>
-                    <p>Телефон: +7 988 234 56 78</p>
-                    <p>Телефон: +7 988 234 56 78</p>
-                </div>
-                <div className='partners'>
-                    <h1>Партнёры</h1>
-                    <div className='logos'>
-                        <img className='logo' src={Shellpea} alt='Shellpea'/>
-                        <img className='logo' src={Dunice} alt='Dunice'/>
-                        <img className='logo' src={Oggetto} alt='Oggetto'/>
+                <div className='group-1'>
+                    <div  className='contacts'>
+                        <h1>Контакты</h1>
+                        <p>Телефон: +7 988 234 56 78</p>
+                        <p>Телефон: +7 988 234 56 78</p>
+                    </div>
+                    <div className='partners'>
+                        <h1>Партнёры</h1>
+                        <div className='logos'>
+                            <img className='logo' src={Shellpea} alt='Shellpea'/>
+                            <img className='logo' src={Dunice} alt='Dunice'/>
+                            <img className='logo' src={Oggetto} alt='Oggetto'/>
+                        </div>
                     </div>
                 </div>
-                <div className='links'>
-                    <h1>Ссылки на нас</h1>
-                    <div className='icons'>
-                        <IoLogoVk className='ico' />
-                        <BiLogoTelegram className='ico'/>
-                        <FaWhatsapp className='ico' /> 
-                    </div>
 
-                </div>
-                <div className='developers'>
-                    <p>Разработано командой</p>
-                    <a><h2>Team404</h2></a>
+                <div className='group-1'>
+                    <div className='links'>
+                        <h1>Ссылки на нас</h1>
+                        <div className='icons'>
+                            <IoLogoVk className='ico' />
+                            <BiLogoTelegram className='ico'/>
+                            <FaWhatsapp className='ico' /> 
+                        </div>
+                    </div>
+                    <div className='developers'>
+                        <p>Разработано командой</p>
+                        <a><h2>Team404</h2></a>
+                    </div>
                 </div>
             </div>
+
             <div className='line1'></div>
             <div className='contetn-right'>
                 <div className='container-left'>
@@ -53,7 +58,7 @@ function Footer() {
                     <div className='map'>
                         <iframe 
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1267.1105390255289!2d38.933639924761984!3d47.2027179868336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40e3fd22a645e35d%3A0xa14e62e43714cfd6!2z0JrQvtGA0L_Rg9GBINCT!5e0!3m2!1sru!2sru!4v1728142645994!5m2!1sru!2sru" 
-                            width="800" 
+                            width="700" 
                             height="475" 
                             style={{border:0}}
                             allowfullscreen="" 
